d8178453b0216f627c01fd594daa780b093f6c3d
Anna <anna.maria.wilczek@gmail.com>
2020-02-06 10:50:01 +0100

    ex2

---
+++ 1/1.Intro/1.Intro/Intro_Excercises/2.Strings.js
@@ -0,0 +1,53 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+var newNumber = Number(numberOne) + Number(numberTwo);
+//console.log(newNumber);
+
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+var one = 10;
+var two = 45;
+var three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+// --------------------------------------
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+var letters = "abc"
+// Get me the character "c"
+
+console.log(letters[2]);
+console.log(letters.charAt(2));
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+var fact = "You are learning javascript!";
+
+var factNew = fact.replace("j","J")
+
+console.log(factNew);
+
+// --------------------------------------
