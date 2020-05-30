5149a5abffb9ade2ddc7417f13918da4e76ad75a
Anna <anna.maria.wilczek@gmail.com>
2020-05-30 22:50:58 +0200

    much more nicer structure

--- Exam_Project/routes/upload.js
+++ Exam_Project/routes/upload.js
@@ -1,9 +1,73 @@
 const router = require("express").Router();
 
-const uploadController = require("../controllers/upload");
-const upload = require("../middleware/upload");
+//const uploadController = require("../controllers/upload");
 
-  router.post("/upload", upload.single("file"), uploadController.uploadFiles);
+const fs = require("fs");
+
+const db = require("../models");
+const Image = db.images;
 
 
-  module.exports = router;
+const multer = require("multer");
+
+const imageFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb("Please upload only images.", false);
+  }
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __basedir + "/uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
+
+router.get("/images", (req,res) => {
+  return res.send({response: images});
+});
+
+
+
+router.post("/upload", upload.single('image'), (req, res) => {
+
+
+   //todo 1.server side validate  
+
+
+   try {
+    console.log(req.file);
+
+    if (req.file == undefined) {
+      return res.send(`You must select a file.`);
+    }
+
+    Image.create({
+      type: req.file.mimetype,
+      image: req.file.originalname,
+      data: fs.readFileSync(
+        __basedir + "/uploads/" + req.file.filename
+      ),
+    }).then((image) => {
+      fs.writeFileSync(
+        __basedir + "/tmp/" + image.name,
+        image.data
+      );
+
+      return res.send(`File has been uploaded.`);
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send(`Error when trying upload images: ${error}`);
+  }
+});
+
+module.exports = router;
